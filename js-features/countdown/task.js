0d4740076e2b83026bac9498ce1c1d69c895176e
baesange <145557904+baesange@users.noreply.github.com>
2026-05-31 13:44:00 +0500

    first commit countdown2

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -2,10 +2,12 @@ let timerElement = document.getElementById('timer');
 let seconds = parseInt(timerElement.textContent);
 
 let interval = setInterval(function() {
-    seconds--;
-    timerElement.textContent = seconds;
+    if (seconds > 0) {
+        seconds--;
+        timerElement.textContent = seconds;
+    }
     
-    if (seconds <= 0) {
+    if (seconds === 0) {
         clearInterval(interval);
         alert('Вы победили в конкурсе!');
     }
