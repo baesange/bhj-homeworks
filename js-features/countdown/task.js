f94226fef4bc23d99adaa0d9f22a2d7cd6f54cc0
baesange <145557904+baesange@users.noreply.github.com>
2026-05-31 13:49:35 +0500

    New 2

--- js-features/countdown/task.js
+++ js-features/countdown/task.js
@@ -1,16 +1,12 @@
-document.addEventListener('DOMContentLoaded', function() {
-    let timerElement = document.getElementById('timer');
-    let seconds = parseInt(timerElement.textContent);
-    
-    let interval = setInterval(function() {
-        if (seconds > 0) {
-            seconds--;
-            timerElement.textContent = seconds;
-        }
-        
-        if (seconds === 0) {
-            clearInterval(interval);
-            alert('Вы победили в конкурсе!');
-        }
-    }, 1000);
-});
+const timerElement = document.getElementById('timer');
+let timeLeft = parseInt(timerElement.textContent);
+
+const countdown = setInterval(() => {
+    timeLeft--;
+    timerElement.textContent = timeLeft;
+
+    if (timeLeft <= 0) {
+        clearInterval(countdown);
+        alert('Вы победили в конкурсе!');
+    }
+}, 1000);
